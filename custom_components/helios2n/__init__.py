27500ef1be5027f2f4f9f293d029a4c1b4cd3438
Bulent Yildiz <ybulent@gmail.com>
2025-08-08 15:20:12 -0400

    init changed

--- custom_components/helios2n/__init__.py
+++ custom_components/helios2n/__init__.py
@@ -1,11 +1,24 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
 from .const import DOMAIN, PLATFORMS
+from .client import LocalHapiClient
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
-    # TODO: create your client from entry.data here and store it
-    # hass.data[DOMAIN][entry.entry_id] = client
+    
+    # Create the client instance using configuration data
+    client = LocalHapiClient(
+        hass=hass,
+        host=entry.data[CONF_HOST],
+        username=entry.data[CONF_USERNAME],
+        password=entry.data[CONF_PASSWORD]
+    )
+    
+    # Store the client instance
+    hass.data[DOMAIN][entry.entry_id] = client
+    
+    # Set up the platform entries
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
